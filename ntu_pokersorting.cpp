c56041d47b2c26c17bdceefe55f378533689859c
hideonatc <stanley013060@gmail.com>
2020-11-24 00:25:09 +0800

    --all

--- ntu_pokersorting.cpp
+++ ntu_pokersorting.cpp
@@ -7,22 +7,22 @@ map<char,int> num,shape;
 string card[13];
 void init(){
 	for(int i=0;i<13;i++)
-		num[num_str[i]]=i+1;
-	for(int i=0;i<4;i++)
-		shape[shape_str[i]]=i+1;
+		num[num_str[i]]=i+1;	//將3到2映射到0到12
+	for(int i=0;i<4;i++)	
+		shape[shape_str[i]]=i+1;	//將花色大小映射到0到3
 	for(int i=0;i<13;i++)
 		cin>>card[i];
 }
 void mysort(){
 	for(int i=0;i<13;i++){
 		for(int j=i;j>0;j--){
-			if(num[card[j][1]]<num[card[j-1][1]]){
+			if(num[card[j][1]]<num[card[j-1][1]]){	//先比較數字大小
 				string swap=card[j];
 				card[j]=card[j-1];
 				card[j-1]=swap;
 			}
 			else if(card[j][1]==card[j-1][1]){
-				if(shape[card[j][0]]<shape[card[j-1][0]]){
+				if(shape[card[j][0]]<shape[card[j-1][0]]){	//若相同再比較花色
 					string swap=card[j];
 					card[j]=card[j-1];
 					card[j-1]=swap;
