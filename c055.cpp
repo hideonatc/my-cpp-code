48f1bc0913bf194dc80e9e03a9c19af054ff9bdd
hideonatc <stanley013060@gmail.com>
2021-07-03 23:46:25 +0800

    --all

--- c055.cpp
+++ c055.cpp
@@ -1,61 +1,58 @@
 #include<iostream>
+#define MAX 100001
 using namespace std;
-int map[1000][1000];
-int dijkstra(int n){
-	int dis[1000]={0};
-	bool vis[1000]={0};
-	for(int i=1;i<=(n-1)*2+1;i++)
+int map[501][501],n;
+int transport[501];
+bool vis[501]={0};
+int dis[501];
+int dijkstra(){
+	for(int i=1;i<=n;i++)
 		dis[i]=map[1][i];
 	vis[1]=1;
-	for(int i=2;i<=(n-1)*2+1;i++){
-		int mn=1e9+1,p;
-		for(int j=1;j<=(n-1)*2+1;j++){
-			if(dis[j]<mn&&!vis[j]){
+	for(int i=2;i<=n;i++){
+		int mn=1e9,p;
+		for(int j=1;j<=n;j++){
+			if(!vis[j]&&dis[j]<mn){
 				mn=dis[j];
 				p=j;
 			}
 		}
-		vis[p]=1;
-		for(int j=1;j<=(n-1)*2+1;j++){
-			if(!vis[j]&&dis[j]>map[p][j]+dis[p])
-				dis[j]=map[p][j]+dis[p];
-		}
+		vis[p]=true;
+		for(int j=1;j<=n;j++)
+			if(!vis[j]&&dis[p]+map[p][j]<dis[j])
+				dis[j]=dis[p]+map[p][j];
 	}
-	if(dis[(n-1)*2+1]>1e8)
-		return -1;
-	return dis[(n-1)*2+1];
+	return dis[n];
 }
-void print_map(int n){
-	for(int i=1;i<=(n-1)*2+1;i++){
-		for(int j=1;j<=(n-1)*2+1;j++){
-			if(map[i][j]==1e9)
-				cout<<"X ";
-			else
-				cout<<map[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	return;
+void print_map(){
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			printf("from %d to %d is %d\n",i,j,map[i][j]);
 }
 int main(){
-	for(int i=0;i<1000;i++)
-		for(int j=0;j<1000;j++)
-			map[i][j]=1e9;
-	int n,m;
 	cin>>n;
-	for(int i=1;i<=n;i++){
-		int in;
-		cin>>in;
+	for(int i=1;i<=n;i++)
 		for(int j=1;j<=n;j++)
-			map[i*2][j*2+1]=in;
-	}
+			map[i][j]=MAX;
+	for(int i=1;i<=n;i++)
+		cin>>transport[i];
+	int m;
 	cin>>m;
 	for(int i=1;i<=m;i++){
 		int a,b,k;
 		cin>>a>>b>>k;
-		if(map[a*2][(b-1)*2+1]>k)
-			map[a*2][(b-1)*2+1]=k;
+		map[a][b]=min(map[a][b],k);
+		map[b][a]=min(map[b][a],k);
 	}
-	print_map(n);
-	cout<<dijkstra(n)<<endl;
+	for(int i=1;i<=n;i++)
+		for(int j=1;j<=n;j++)
+			map[j][i]+=transport[i];
+	for(int i=1;i<=n;i++)
+		map[i][i]=0;
+	//print_map();
+	int ans=dijkstra();
+	if(ans>=MAX)
+		cout<<"-1"<<endl;
+	else
+		cout<<ans<<endl;
 }
